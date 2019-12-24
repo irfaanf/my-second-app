1bbf6c93c3b31dff3a249206b7bbfbf9a34eca43
irfaan.fakir <irfaan@psybergate.co.za>
2019-12-24 19:59:53 +0200

    Add components using the CLI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { PopupComponent } from './menu/popup/popup.component';
+import { AdvertComponent } from './advert/advert.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PopupComponent,
+    AdvertComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
