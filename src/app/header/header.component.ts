727c2386344d0253459b87aa07642217cc14b88e
irfaan.fakir <irfaan@psybergate.co.za>
2020-01-07 12:59:56 +0200

    Ways to use ngClass.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,17 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   showGreeting: boolean = false;
-  
+
   isMorning = false;
 
-  customButtonClass = 'btn';
-  customButtonMainClass = 'btn-primary';
+  customButtonClass = 'btn btn-sm btn-warning';
+  customButtonMainClass = 'btn';
+  customButtonSecondaryClass = 'btn-success';
 
   manualClass = 'btn btn-info';
+
+  specialObject = {
+    'btn': true,
+    'btn-info': true
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  calculateClasses() {
+    return {
+      'btn': true,
+      'btn-secondary': true
+    }
+  }
+
+  returnString() {
+    return 'btn btn-danger';
+  }
+
+  returnArray() {
+    return ['btn', 'btn-success'];
+  }
+
+  returnObject() {
+    return { 'btn': true, 'btn-info': true };
+  }
 }
